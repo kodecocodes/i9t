6b6b6aab20834f83909fafa4e7fd9540c039fd67
Evan Dekhayser <erdekhayser@gmail.com>
2015-06-25 19:36:08 -0400

    Reformatted using spaces, not tabs

--- contacts/projects/final/RWConnect/RWConnect/Friend.swift
+++ contacts/projects/final/RWConnect/RWConnect/Friend.swift
@@ -10,22 +10,21 @@ import UIKit
 
 // Starter Code
 struct Friend{
-	let firstName: String
-	let lastName: String
-	let workEmail: String
-	let profilePicture: UIImage?
-	
-	init(firstName: String, lastName: String, workEmail: String, profilePicture: UIImage?){
-		self.firstName = firstName
-		self.lastName = lastName
-		self.workEmail = workEmail
-		self.profilePicture = profilePicture
-	}
-	
-	static func defaultContacts() -> [Friend] {
-		return [Friend(firstName: "Mic", lastName: "Pringle", workEmail: "mic@example.com", profilePicture: UIImage(named: "MicProfilePicture")), Friend(firstName: "Ray", lastName: "Wenderlich", workEmail: "ray@example.com", profilePicture: UIImage(named: "RayProfilePicture")), Friend(firstName: "Sam", lastName: "Davies", workEmail: "sam@example.com", profilePicture: UIImage(named: "SamProfilePicture")), Friend(firstName: "Greg", lastName: "Heo", workEmail: "greg@example.com", profilePicture: UIImage(named: "GregProfilePicture"))]
-	}
-	
+  let firstName: String
+  let lastName: String
+  let workEmail: String
+  let profilePicture: UIImage?
+  
+  init(firstName: String, lastName: String, workEmail: String, profilePicture: UIImage?){
+    self.firstName = firstName
+    self.lastName = lastName
+    self.workEmail = workEmail
+    self.profilePicture = profilePicture
+  }
+  
+  static func defaultContacts() -> [Friend] {
+    return [Friend(firstName: "Mic", lastName: "Pringle", workEmail: "mic@example.com", profilePicture: UIImage(named: "MicProfilePicture")), Friend(firstName: "Ray", lastName: "Wenderlich", workEmail: "ray@example.com", profilePicture: UIImage(named: "RayProfilePicture")), Friend(firstName: "Sam", lastName: "Davies", workEmail: "sam@example.com", profilePicture: UIImage(named: "SamProfilePicture")), Friend(firstName: "Greg", lastName: "Heo", workEmail: "greg@example.com", profilePicture: UIImage(named: "GregProfilePicture"))]
+  }
 }
 
 // Added during the tutorial
@@ -34,26 +33,26 @@ struct Friend{
 import Contacts
 
 extension Friend{
-	init(contact: CNContact){
-		firstName = contact.givenName
-		lastName = contact.familyName
-		workEmail = contact.emailAddresses[0].value as! String
-		if let imageData = contact.imageData{
-			profilePicture = UIImage(data: imageData)
-		} else {
-			profilePicture = nil
-		}
-	}
-	
-	var contactValue: CNMutableContact{
-		let contact = CNMutableContact()
-		contact.givenName = firstName
-		contact.familyName = lastName
-		contact.emailAddresses = [CNLabeledValue(label: CNLabelWork, value: workEmail)]
-		if let profilePicture = profilePicture{
-			let imageData = UIImageJPEGRepresentation(profilePicture, 1)
-			contact.imageData = imageData
-		}
-		return contact
-	}
+  init(contact: CNContact){
+    firstName = contact.givenName
+    lastName = contact.familyName
+    workEmail = contact.emailAddresses[0].value as! String
+    if let imageData = contact.imageData{
+      profilePicture = UIImage(data: imageData)
+    } else {
+      profilePicture = nil
+    }
+  }
+  
+  var contactValue: CNMutableContact{
+    let contact = CNMutableContact()
+    contact.givenName = firstName
+    contact.familyName = lastName
+    contact.emailAddresses = [CNLabeledValue(label: CNLabelWork, value: workEmail)]
+    if let profilePicture = profilePicture{
+      let imageData = UIImageJPEGRepresentation(profilePicture, 1)
+      contact.imageData = imageData
+    }
+    return contact
+  }
 }
--- contacts/projects/final/RWConnect/RWConnect/FriendsViewController.swift
+++ contacts/projects/final/RWConnect/RWConnect/FriendsViewController.swift
@@ -11,33 +11,33 @@ import UIKit
 // Starter Code
 
 class FriendsViewController: UITableViewController {
-	
-	var friendsList = Friend.defaultContacts()
-	
-	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		return 1
-	}
-	
-	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return friendsList.count
-	}
-	
-	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCellWithIdentifier("FriendCell")!
-		
-		let friend = friendsList[indexPath.row]
-		cell.textLabel?.text = friend.firstName + " " + friend.lastName
-		cell.detailTextLabel?.text = friend.workEmail
-		cell.imageView?.image = friend.profilePicture
-		
-		return cell
-	}
-
-	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {}
-	
+  
+  var friendsList = Friend.defaultContacts()
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return friendsList.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("FriendCell")!
+    
+    let friend = friendsList[indexPath.row]
+    cell.textLabel?.text = friend.firstName + " " + friend.lastName
+    cell.detailTextLabel?.text = friend.workEmail
+    cell.imageView?.image = friend.profilePicture
+    
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {}
+  
 }
 
-// ALL CODE UNDERNEATH: Added during throughout the chapter, in order 
+// ALL CODE UNDERNEATH: Added during throughout the chapter, in order
 
 // imports will be added to the top of the file in the chapter
 import Contacts
@@ -45,127 +45,127 @@ import ContactsUI
 
 // Show Contact Info on Accessory Press
 extension FriendsViewController{
-	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-		tableView.deselectRowAtIndexPath(indexPath, animated: true)
-		let friend = friendsList[indexPath.row]
-		let contact = friend.contactValue
-		let contactViewController = CNContactViewController(forUnknownContact: contact)
-		contactViewController.allowsEditing = false
-		contactViewController.allowsActions = false
-		contactViewController.edgesForExtendedLayout = .None
-		navigationController?.pushViewController(contactViewController, animated: true)
-	}
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    let friend = friendsList[indexPath.row]
+    let contact = friend.contactValue
+    let contactViewController = CNContactViewController(forUnknownContact: contact)
+    contactViewController.allowsEditing = false
+    contactViewController.allowsActions = false
+    contactViewController.edgesForExtendedLayout = .None
+    navigationController?.pushViewController(contactViewController, animated: true)
+  }
 }
 
 // Import Friends from Contacts
 extension FriendsViewController{
-	@IBAction func addFriends(sender: UIBarButtonItem) {
-		let contactPicker = CNContactPickerViewController()
-		contactPicker.predicateForEnablingContact = NSPredicate(format: "emailAddresses.@count > 0", argumentArray: nil)
-		contactPicker.delegate = self
-		presentViewController(contactPicker, animated: true, completion: nil)
-	}
+  @IBAction func addFriends(sender: UIBarButtonItem) {
+    let contactPicker = CNContactPickerViewController()
+    contactPicker.predicateForEnablingContact = NSPredicate(format: "emailAddresses.@count > 0", argumentArray: nil)
+    contactPicker.delegate = self
+    presentViewController(contactPicker, animated: true, completion: nil)
+  }
 }
 
 extension FriendsViewController: CNContactPickerDelegate{
-	func contactPicker(picker: CNContactPickerViewController, didSelectContacts contacts: [CNContact]) {
-		let newFriends = contacts.map{ Friend(contact: $0) }
-		friendsList += newFriends
-		tableView.reloadData()
-	}
+  func contactPicker(picker: CNContactPickerViewController, didSelectContacts contacts: [CNContact]) {
+    let newFriends = contacts.map{ Friend(contact: $0) }
+    friendsList += newFriends
+    tableView.reloadData()
+  }
 }
 
 // Create New Contacts from Friends
 extension FriendsViewController{
-	override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
-		let createContact = UITableViewRowAction(style: .Normal, title: "Create Contact") { rowAction, indexPath in
-			
-			tableView.setEditing(false, animated: true)
-			
-			let contactStore = CNContactStore()
-			contactStore.requestAccessForEntityType(CNEntityType.Contacts) { userGrantedAccess, _ in
-				guard userGrantedAccess else {
-					self.presentPermissionErrorAlert()
-					return
-				}
-				
-				let friend = self.friendsList[indexPath.row]
-				let contactFormatter = CNContactFormatter()
-				let predicateForMatchingName = CNContact.predicateForContactsMatchingName(contactFormatter.stringFromContact(friend.contactValue)!)
-				let matchingContacts = try! contactStore.unifiedContactsMatchingPredicate(predicateForMatchingName, keysToFetch: [CNContactEmailAddressesKey, CNContactImageDataKey])
-				if let firstMatch = matchingContacts.first{
-					self.updateContact(firstMatch.mutableCopy() as! CNMutableContact, withFriend: friend)
-				} else {
-					self.saveFriendToContacts(friend)
-				}
-			}
-		}
-		createContact.backgroundColor = rwGreen
-		return [createContact]
-	}
-	
-	func saveFriendToContacts(friend: Friend){
-		let contact = friend.contactValue
-		let saveRequest = CNSaveRequest()
-		saveRequest.addContact(contact, toContainerWithIdentifier: nil)
-		
-		handleSaveRequest(saveRequest)
-	}
-	
-	func updateContact(contact: CNContact, withFriend friend: Friend){
-		let mutableContact = contact as! CNMutableContact
-		let workEmail = CNLabeledValue(label: CNLabelWork, value: friend.workEmail)
-		
-		if !contact.emailAddresses.contains(workEmail) {
-			mutableContact.emailAddresses.append(workEmail)
-		}
-		
-		if let profilePicture = friend.profilePicture{
-			mutableContact.imageData = UIImageJPEGRepresentation(profilePicture, 1)
-		}
-		
-		let saveRequest = CNSaveRequest()
-		saveRequest.updateContact(mutableContact)
-		handleSaveRequest(saveRequest)
-	}
-	
-	func handleSaveRequest(saveRequest: CNSaveRequest){
-		let contactStore = CNContactStore()
-		do{
-			try contactStore.executeSaveRequest(saveRequest)
-			let successAlert = UIAlertController(title: "Contacts Saved", message: nil, preferredStyle: .Alert)
-			successAlert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-			dispatch_async(dispatch_get_main_queue()){
-				self.presentViewController(successAlert, animated: true, completion: nil)
-			}
-		} catch {
-			presentSavingErrorAlert()
-		}
-	}
-	
-	func presentSavingErrorAlert(){
-		dispatch_async(dispatch_get_main_queue()){
-			let alert = UIAlertController(title: "Could Not Save Contact", message: "An unknown error occurred.", preferredStyle: .Alert)
-
-			let dismissAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
-			alert.addAction(dismissAction)
-			
-			self.presentViewController(alert, animated: true, completion: nil)
-		}
-	}
-	
-	func presentPermissionErrorAlert(){
-		dispatch_async(dispatch_get_main_queue()){
-			let alert = UIAlertController(title: "Could Not Save Contact", message: "How am I supposed to add the contact if you didn't give me permission?", preferredStyle: .Alert)
-			
-			let openSettingsAction = UIAlertAction(title: "Settings", style: .Default, handler: { alert in
-				UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
-			})
-			let dismissAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
-			alert.addAction(openSettingsAction)
-			alert.addAction(dismissAction)
-			self.presentViewController(alert, animated: true, completion: nil)
-		}
-	}
+  override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+    let createContact = UITableViewRowAction(style: .Normal, title: "Create Contact") { rowAction, indexPath in
+      
+      tableView.setEditing(false, animated: true)
+      
+      let contactStore = CNContactStore()
+      contactStore.requestAccessForEntityType(CNEntityType.Contacts) { userGrantedAccess, _ in
+        guard userGrantedAccess else {
+          self.presentPermissionErrorAlert()
+          return
+        }
+        
+        let friend = self.friendsList[indexPath.row]
+        let contactFormatter = CNContactFormatter()
+        let predicateForMatchingName = CNContact.predicateForContactsMatchingName(contactFormatter.stringFromContact(friend.contactValue)!)
+        let matchingContacts = try! contactStore.unifiedContactsMatchingPredicate(predicateForMatchingName, keysToFetch: [CNContactEmailAddressesKey, CNContactImageDataKey])
+        if let firstMatch = matchingContacts.first{
+          self.updateContact(firstMatch.mutableCopy() as! CNMutableContact, withFriend: friend)
+        } else {
+          self.saveFriendToContacts(friend)
+        }
+      }
+    }
+    createContact.backgroundColor = rwGreen
+    return [createContact]
+  }
+  
+  func saveFriendToContacts(friend: Friend){
+    let contact = friend.contactValue
+    let saveRequest = CNSaveRequest()
+    saveRequest.addContact(contact, toContainerWithIdentifier: nil)
+    
+    handleSaveRequest(saveRequest)
+  }
+  
+  func updateContact(contact: CNContact, withFriend friend: Friend){
+    let mutableContact = contact as! CNMutableContact
+    let workEmail = CNLabeledValue(label: CNLabelWork, value: friend.workEmail)
+    
+    if !contact.emailAddresses.contains(workEmail) {
+      mutableContact.emailAddresses.append(workEmail)
+    }
+    
+    if let profilePicture = friend.profilePicture{
+      mutableContact.imageData = UIImageJPEGRepresentation(profilePicture, 1)
+    }
+    
+    let saveRequest = CNSaveRequest()
+    saveRequest.updateContact(mutableContact)
+    handleSaveRequest(saveRequest)
+  }
+  
+  func handleSaveRequest(saveRequest: CNSaveRequest){
+    let contactStore = CNContactStore()
+    do{
+      try contactStore.executeSaveRequest(saveRequest)
+      let successAlert = UIAlertController(title: "Contacts Saved", message: nil, preferredStyle: .Alert)
+      successAlert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+      dispatch_async(dispatch_get_main_queue()){
+        self.presentViewController(successAlert, animated: true, completion: nil)
+      }
+    } catch {
+      presentSavingErrorAlert()
+    }
+  }
+  
+  func presentSavingErrorAlert(){
+    dispatch_async(dispatch_get_main_queue()){
+      let alert = UIAlertController(title: "Could Not Save Contact", message: "An unknown error occurred.", preferredStyle: .Alert)
+      
+      let dismissAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+      alert.addAction(dismissAction)
+      
+      self.presentViewController(alert, animated: true, completion: nil)
+    }
+  }
+  
+  func presentPermissionErrorAlert(){
+    dispatch_async(dispatch_get_main_queue()){
+      let alert = UIAlertController(title: "Could Not Save Contact", message: "How am I supposed to add the contact if you didn't give me permission?", preferredStyle: .Alert)
+      
+      let openSettingsAction = UIAlertAction(title: "Settings", style: .Default, handler: { alert in
+        UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
+      })
+      let dismissAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+      alert.addAction(openSettingsAction)
+      alert.addAction(dismissAction)
+      self.presentViewController(alert, animated: true, completion: nil)
+    }
+  }
 }
 
