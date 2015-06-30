1a84c5c521dfa361814ab3ce82bec1e6d61eccdd
Soheil M. Azarpour <soheil.moayedi@gmail.com>
2015-06-30 00:46:21 -0400

    Add a new custom cell.

---
+++ multitasking/projects/final/Travelog/Travelog/Custom Views/LogCell.swift
@@ -0,0 +1,52 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import TravelogKit
+
+class LogCell: UITableViewCell {
+  
+  @IBOutlet private var customTextLabel: UILabel!
+  @IBOutlet private var customImageView: UIImageView!
+  
+  func setLog(log: BaseLog?) {
+    // If it's a textLog, update label and hide image view.
+    // If it's an image log, update image view and hide label.
+    // If it's neither of them, hide both label and image view.
+    if let log = log as? TextLog {
+      customTextLabel.hidden = false
+      customImageView.hidden = true
+      customTextLabel.text = log.text
+      customImageView.image = nil
+    } else if let log = log as? ImageLog {
+      customTextLabel.hidden = true
+      customImageView.hidden = false
+      customTextLabel.text = nil
+      customImageView.image = log.image
+    } else {
+      customTextLabel.hidden = true
+      customImageView.hidden = true
+      customTextLabel.text = nil
+      customImageView.image = nil
+    }
+  }
+}
