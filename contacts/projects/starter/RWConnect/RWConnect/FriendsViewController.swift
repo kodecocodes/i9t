f3be0d39396a0b08f52eb1f4162baa47ee683ca3
richard.turton <richard.turton@mubaloo.com>
2015-07-12 21:10:49 +0100

    Separated protocol methods into a class extension

--- contacts/projects/starter/RWConnect/RWConnect/FriendsViewController.swift
+++ contacts/projects/starter/RWConnect/RWConnect/FriendsViewController.swift
@@ -27,26 +27,31 @@ import ContactsUI
 class FriendsViewController: UITableViewController {
 	
 	var friendsList = Friend.defaultContacts()
-	
-	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		return 1
-	}
-	
-	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return friendsList.count
-	}
-	
-	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCellWithIdentifier("FriendCell")!
-		
-		let friend = friendsList[indexPath.row]
-		cell.textLabel?.text = friend.firstName + " " + friend.lastName
-		cell.detailTextLabel?.text = friend.workEmail
-		cell.imageView?.image = friend.profilePicture
-		
-		return cell
-	}
-	
-	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {}
-	
+
+}
+
+//MARK: UITableViewDataSource
+extension FriendsViewController {
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendsList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FriendCell")!
+        
+        let friend = friendsList[indexPath.row]
+        cell.textLabel?.text = friend.firstName + " " + friend.lastName
+        cell.detailTextLabel?.text = friend.workEmail
+        cell.imageView?.image = friend.profilePicture
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {}
+    
 }
