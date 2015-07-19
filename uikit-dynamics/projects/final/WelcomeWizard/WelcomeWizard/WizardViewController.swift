755cff32eac1077af94e371e6ab3b2c3bb17b83a
Aaron Douglas <astralbodies@gmail.com>
2015-07-19 11:45:38 -0500

    Added wizard VC class

---
+++ uikit-dynamics/projects/final/WelcomeWizard/WelcomeWizard/WizardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WizardViewController.swift
+//  WelcomeWizard
+//
+//  Created by Aaron Douglas on 7/19/15.
+//  Copyright © 2015 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class WizardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
