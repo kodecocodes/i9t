27deb14ca4afca1babc8038f923634c9c58fd740
Jeff Rames <jeff@floodlightsoftware.com>
2015-07-25 23:12:38 -0500

    removed some unnecessary semi-colons

--- app-thinning/projects/starter/Old CA Maps/Old CA Maps/HistoricTileMapOverlayRenderer.swift
+++ app-thinning/projects/starter/Old CA Maps/Old CA Maps/HistoricTileMapOverlayRenderer.swift
@@ -33,14 +33,14 @@ class HistoricTileMapOverlayRenderer: MKTileOverlayRenderer {
     for tile in tilesInRect {
       let rect = rectForMapRect(tile.mapRect)
       let image = UIImage(contentsOfFile: tile.imagePath)!
-      CGContextSaveGState(context);
-      CGContextTranslateCTM(context, CGRectGetMinX(rect), CGRectGetMinY(rect));
-      CGContextScaleCTM(context, 1/zoomScale, 1/zoomScale);
-      CGContextTranslateCTM(context, 0, image.size.height);
-      CGContextScaleCTM(context, 1, -1);
+      CGContextSaveGState(context)
+      CGContextTranslateCTM(context, CGRectGetMinX(rect), CGRectGetMinY(rect))
+      CGContextScaleCTM(context, 1/zoomScale, 1/zoomScale)
+      CGContextTranslateCTM(context, 0, image.size.height)
+      CGContextScaleCTM(context, 1, -1)
       let drawRect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
-      CGContextDrawImage(context, drawRect, image.CGImage);
-      CGContextRestoreGState(context);
+      CGContextDrawImage(context, drawRect, image.CGImage)
+      CGContextRestoreGState(context)
     }
   }
   
