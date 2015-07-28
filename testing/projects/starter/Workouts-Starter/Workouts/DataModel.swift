f910df80620ecb7645b682149cdd0610d4c13e56
Pietro Rea <pietorea@gmail.com>
2015-07-28 08:06:13 -0400

    Starter project: added copyright notice + fixed indentation

--- testing/projects/starter/Workouts-Starter/Workouts/DataModel.swift
+++ testing/projects/starter/Workouts-Starter/Workouts/DataModel.swift
@@ -1,10 +1,24 @@
-//
-//  DataModel.swift
-//  Workouts
-//
-//  Created by Pietro Rea on 7/24/15.
-//  Copyright © 2015 Razeware. All rights reserved.
-//
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import Foundation
 
@@ -23,7 +37,7 @@ class DataModel {
     }()
   
   init() {
-   
+    
     if NSFileManager.defaultManager().fileExistsAtPath(dataFilePath()) {
       
       let data = NSData(contentsOfFile: dataFilePath())
@@ -73,7 +87,7 @@ class DataModel {
     archiver.finishEncoding()
     
     if !data.writeToFile(dataFilePath(), atomically: true) {
-     // error
+      // error
     }
   }
   
@@ -85,7 +99,7 @@ class DataModel {
   }
   
   private func addTestData() {
-  
+    
     //Exercises
     
     //jumping jacks
@@ -157,7 +171,7 @@ class DataModel {
     tricepsDips.duration = 30
     
     addExercise(tricepsDips)
-
+    
     //plank
     let plank = Exercise()
     plank.userCreated = false
--- testing/projects/starter/Workouts-Starter/Workouts/ExerciseViewController.swift
+++ testing/projects/starter/Workouts-Starter/Workouts/ExerciseViewController.swift
@@ -1,10 +1,24 @@
-//
-//  ExerciseViewController.swift
-//  Workouts
-//
-//  Created by Pietro Rea on 7/22/15.
-//  Copyright (c) 2015 Razeware. All rights reserved.
-//
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import UIKit
 
@@ -14,7 +28,7 @@ private let addExerciseNewIdentifier = "AddNewExerciseCell"
 private let toDetailSegue = "toExerciseDetailViewController"
 
 class ExerciseViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+  
   @IBOutlet weak var editButton: UIBarButtonItem!
   @IBOutlet weak var tableView: UITableView!
   let dataModel = DataModel()
@@ -23,7 +37,7 @@ class ExerciseViewController: UIViewController, UITableViewDataSource, UITableVi
     super.viewDidLoad()
     
   }
-
+  
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
     
@@ -138,7 +152,7 @@ class ExerciseViewController: UIViewController, UITableViewDataSource, UITableVi
   }
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-   
+    
     if segue.identifier == toDetailSegue {
       let indexPath = sender as! NSIndexPath
       let detailViewController = segue.destinationViewController as! ExerciseDetailViewController
