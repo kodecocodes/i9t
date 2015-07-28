c0a96fbd5a1032713e31686e532c08cad9611587
vincentngo <vincentn@vt.edu>
2015-07-27 22:13:48 -0500

    Move project to github

---
+++ location-and-mapping/projects/final/CafeTransit/CafeTransit/CoffeeShop.swift
@@ -0,0 +1,159 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import CoreLocation
+
+enum PriceGuide : Int {
+    case Unknown = 0
+    case Low = 1
+    case Medium = 2
+    case High = 3
+}
+
+extension PriceGuide : CustomStringConvertible {
+    var description : String {
+        switch self {
+        case .Unknown:
+            return "?"
+        case .Low:
+            return "💰"
+        case .Medium:
+            return "💰💰"
+        case .High:
+            return "💰💰💰"
+        }
+    }
+}
+
+enum CoffeeRating {
+    case Unknown
+    case Rating(Int)
+}
+
+extension CoffeeRating {
+    init?(value: Int) {
+        if value > 0 && value <= 5 {
+            self = .Rating(value)
+        } else {
+            self = .Unknown
+        }
+    }
+}
+
+extension CoffeeRating : CustomStringConvertible {
+    var description : String {
+        switch self {
+        case .Unknown:
+            return ""
+        case .Rating(let value):
+            var rating = ""
+            for var index = 0; index < value; ++index {
+                rating += "★"
+            }
+            return rating
+        }
+    }
+}
+
+struct CoffeeShop {
+	let name: String
+	let photo: UIImage?
+	let priceGuide: PriceGuide
+	let location: CLLocationCoordinate2D?
+	let details: String
+	let rating: CoffeeRating
+    let yelpWebsite: String
+    let startTime: NSDate?
+    let endTime: NSDate?
+    let phone: String
+}
+
+extension CoffeeShop {
+	init?(dict: [String : AnyObject]) {
+		guard let name = dict["name"] as? String,
+            let phone = dict["phone"] as? String,
+            let yelpWebsite = dict["yelpWebsite"] as? String,
+			let priceGuideRaw = dict["priceGuide"] as? Int,
+			let priceGuide = PriceGuide(rawValue: priceGuideRaw),
+			let details = dict["details"] as? String,
+			let ratingRaw = dict["rating"] as? Int,
+			let rating = CoffeeRating(value: ratingRaw) else {
+				return nil
+		}
+		
+		self.name = name
+        self.phone = phone
+        self.yelpWebsite = yelpWebsite
+		self.priceGuide = priceGuide
+		self.details = details
+		self.rating = rating
+		
+		if let imageName = dict["imageName"] as? String where !imageName.isEmpty {
+			photo = UIImage(named: imageName)
+		} else {
+			photo = nil
+		}
+		
+		if let latitude = dict["latitude"] as? Double,
+			let longitude = dict["longitude"] as? Double {
+				location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+		} else {
+			location = nil
+		}
+        
+        if let openTime = dict["openTime"] as? NSDate {
+            startTime = openTime
+        } else {
+            startTime = nil
+        }
+        
+        if let closeTime = dict["closeTime"] as? NSDate {
+            endTime = closeTime
+        } else {
+            endTime = nil
+        }
+	}
+}
+
+extension CoffeeShop {
+	static func loadDefaultCoffeeShops() -> [CoffeeShop]? {
+		return loadCoffeeShopsFromPlistNamed("sanfrancisco_coffeeshops")
+	}
+	
+	static func loadCoffeeShopsFromPlistNamed(plistName: String) -> [CoffeeShop]? {
+		guard let path = NSBundle.mainBundle().pathForResource(plistName, ofType: "plist"),
+			let array = NSArray(contentsOfFile: path) as? [[String : AnyObject]] else {
+				return nil
+		}
+		
+		return array.map { CoffeeShop(dict: $0) }
+			.filter { $0 != nil }
+			.map { $0! }
+	}
+}
+
+extension CoffeeShop : CustomStringConvertible {
+	var description : String {
+		return "\(name) :: \(details)"
+	}
+}
---
+++ location-and-mapping/projects/final/CafeTransit/CafeTransit/CoffeeShopPinDetailView.swift
@@ -0,0 +1,127 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import MapKit
+
+class CoffeeShopPinDetailView : UIView {
+    @IBOutlet var ratingsLabel: UILabel!
+    @IBOutlet var hoursLabel: UILabel!
+    @IBOutlet var priceGuide: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var departureLabel: UILabel!
+    @IBOutlet var arrivalLabel: UILabel!
+    @IBOutlet var travelTimeLabel: UILabel!
+    
+    var coffeeShop: CoffeeShop?
+    
+	var view: UIView!
+	var nibName: String = "CoffeeShopPinDetailView"
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+    
+    override func awakeFromNib() {
+    }
+	
+    func updateDetailView(coffeeShop: CoffeeShop) {
+        self.coffeeShop = coffeeShop
+        ratingsLabel.text = coffeeShop.rating.description
+        descriptionLabel.text = coffeeShop.details
+        priceGuide.text = coffeeShop.priceGuide.description
+
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        let startTime = dateFormatter.stringFromDate(coffeeShop.startTime!)
+        let endTime = dateFormatter.stringFromDate(coffeeShop.endTime!)
+        hoursLabel.text = "Time: \(startTime) - \(endTime)"
+    }
+    
+    @IBAction func phoneTapped(sender: AnyObject) {
+        if let phone = self.coffeeShop?.phone {
+            let phoneString = "tel://" + phone
+            if let url = NSURL(string: phoneString) {
+                UIApplication.sharedApplication().openURL(url)
+            }
+        }
+    }
+    
+    @IBAction func transitTapped(sender: AnyObject) {
+        if let location = self.coffeeShop?.location {
+            openInMapTransit(location)
+        }
+    }
+    
+    @IBAction func internetTapped(sender: AnyObject) {
+        if let website = self.coffeeShop?.yelpWebsite {
+            UIApplication.sharedApplication().openURL(NSURL(string: website)!)
+        }
+    }
+    
+    //MARK: Transit Helpers
+    func openInMapTransit(coord:CLLocationCoordinate2D) {
+        let placemark = MKPlacemark(coordinate: coord, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeTransit]
+        mapItem.openInMapsWithLaunchOptions(launchOptions)
+    }
+    
+    func setTransitEstimatedTimes(currentLocation: CLLocationCoordinate2D) {
+        let request = MKDirectionsRequest()
+        
+        let source = MKMapItem(placemark: MKPlacemark(coordinate: currentLocation, addressDictionary: nil))
+        let destination = MKMapItem(placemark: MKPlacemark(coordinate: (self.coffeeShop?.location)!, addressDictionary: nil))
+        request.source = source
+        request.destination = destination
+        //Set Transport Type to be Transit
+        request.transportType = MKDirectionsTransportType.Transit
+		let directions = MKDirections(request: request)
+        directions.calculateETAWithCompletionHandler { response, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                let travelTime = String(format: "%d min away by public transport.", Int(response!.expectedTravelTime / 60))
+                let arrivalTime = String(format: "Arrive at: %@", response!.expectedArrivalDate)
+                let departureTime = String(format: "Depart at: %@", response!.expectedDepartureDate)
+                self.departureLabel.text = departureTime
+                self.arrivalLabel.text = arrivalTime
+                self.travelTimeLabel.text = travelTime
+ 
+            }
+        }
+    }
+}
+
+extension UIView {
+	class func loadFromNibNamed(nibNamed: String, bundle : NSBundle? = nil) -> UIView? {
+		return UINib(
+			nibName: nibNamed,
+			bundle: bundle
+			).instantiateWithOwner(nil, options: nil)[0] as? UIView
+	}
+}
