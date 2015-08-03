031d50624703a0bbfb7ba088aedf64991a4c83b1
Aaron Douglas <astralbodies@gmail.com>
2015-08-02 19:41:53 -0500

    Accidentally checked in a starter change

--- uikit-dynamics/projects/starter/DynamicPhotoDisplay/DynamicPhotoDisplay/StickyEdgesBehavior.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StickyEdgeBehaviors.swift
-//  DynamicPhotoDisplay
-//
-//  Created by Aaron Douglas on 8/2/15.
-//  Copyright © 2015 Razeware LLC. All rights reserved.
-//
-
-import Foundation
