4d1d53a20e5a50599827ee78337ba873225564e6
Chris Wagner <cwagner118@gmail.com>
2015-08-05 22:03:31 -0700

    cp final to starter

---
+++ introducing-app-search/projects/starter/Colleagues/Colleagues/AppDelegate.swift
@@ -0,0 +1,75 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import EmployeeKit
+import CoreSpotlight
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(application: UIApplication, continueUserActivity userActivity: NSUserActivity, restorationHandler: ([AnyObject]?) -> Void) -> Bool {
+    
+    let objectId: String?
+    
+    if userActivity.activityType == Employee.domainIdentifier, let activityObjectId = userActivity.userInfo?["id"] as? String {
+      objectId = activityObjectId
+    } else if userActivity.activityType == CSSearchableItemActionType,
+      let activityObjectId = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String  {
+      objectId = activityObjectId
+    } else {
+      objectId = nil
+    }
+    
+    if let objectId = objectId,
+      navController = window?.rootViewController as? UINavigationController,
+      employeeListViewController = navController.viewControllers.first as? EmployeeListViewController,
+      employee = EmployeeService().employeeWithObjectId(objectId)
+    {
+      navController.popToRootViewControllerAnimated(false)
+      
+      let employeeViewController = employeeListViewController.storyboard?.instantiateViewControllerWithIdentifier("EmployeeView") as! EmployeeViewController
+      employeeViewController.employee = employee
+      navController.pushViewController(employeeViewController, animated: false)
+    }
+    
+    return true
+  }
+  
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+  
+    switch Setting.searchIndexingPreference {
+    case .Disabled:
+      EmployeeService().destroyEmployeeIndexing()
+    case .AllRecords:
+      EmployeeService().indexAllEmployees()
+    default: break
+    }
+    
+    return true
+  }
+  
+}
+
---
+++ introducing-app-search/projects/starter/Colleagues/Colleagues/EmployeeExt.swift
@@ -0,0 +1,64 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import Foundation
+import EmployeeKit
+import CoreSpotlight
+import MobileCoreServices
+
+extension Employee {
+  
+  var userActivityUserInfo: [NSObject: AnyObject] {
+    return ["id": objectId]
+  }
+  
+  var userActivity: NSUserActivity {
+    let activity = NSUserActivity(activityType: self.dynamicType.domainIdentifier)
+    activity.title = name
+    switch Setting.searchIndexingPreference {
+    case .Disabled:
+      activity.eligibleForSearch = false
+    default:
+      activity.eligibleForSearch = true
+    }
+    
+    activity.userInfo = userActivityUserInfo
+    activity.keywords = [email, department]
+        
+    activity.contentAttributeSet = searchableItemAttributeSet
+    return activity
+  }
+  
+  /// Dial the employee's phone number use the system dialer.
+  func call() {
+    let sanitizedPhoneNumber = phone.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+    let callUrl = NSURL(string: "tel:" + sanitizedPhoneNumber)!
+    UIApplication.sharedApplication().openURL(callUrl)
+  }
+  
+  /// Open the system email client with the employee's email address populated in the TO field.
+  func sendEmail() {
+    let mailUrl = NSURL(string: "mailto:" + email)!
+    UIApplication.sharedApplication().openURL(mailUrl)
+  }
+  
+}
