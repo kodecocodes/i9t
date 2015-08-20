f24bfa85efda8b81303769c92be3b700b72790cd
carolight <caroline@archway.com.au>
2015-08-20 18:51:46 +0100

    Paws Background Centre Align for address details

---
+++ custom-segues/projects/starter/PamperedPets/PamperedPets/BackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundView.swift
+//  PamperedPets
+//
+//  Created by Caroline Begbie on 20/08/2015.
+//  Copyright © 2015 Caroline Begbie. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
