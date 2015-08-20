ad8d50a8b033d1b7e5b1f41186d123337ab94a5f
carolight <caroline@archway.com.au>
2015-08-20 18:52:27 +0100

    (Forgot to add unstaged files) Paws Background Centre Align for address details

--- custom-segues/projects/starter/PamperedPets/PamperedPets/BackgroundView.swift
+++ custom-segues/projects/starter/PamperedPets/PamperedPets/BackgroundView.swift
@@ -1,21 +1,42 @@
-//
-//  BackgroundView.swift
-//  PamperedPets
-//
-//  Created by Caroline Begbie on 20/08/2015.
-//  Copyright © 2015 Caroline Begbie. All rights reserved.
-//
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import UIKit
 
+@IBDesignable
+
 class BackgroundView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+  
+  @IBInspectable var image:UIImage? {
+    didSet {
+      setNeedsDisplay()
     }
-    */
-
+  }
+  
+  override func drawRect(rect: CGRect) {
+    if let image = image {
+      let context = UIGraphicsGetCurrentContext()
+      UIColor(patternImage: image).setFill()
+      CGContextFillRect(context, rect)
+    }
+  }
 }
