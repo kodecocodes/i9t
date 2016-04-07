270576eba9e86952c615f5733312b51e8cb6f06c
Derek Selander <derekselander@yahoo.com>
2016-04-06 22:32:52 -0600

    Starter project updates

--- app-thinning/projects/starter/Old CA Maps/Old CA Maps/HistoricTileMapOverlay.swift
+++ app-thinning/projects/starter/Old CA Maps/Old CA Maps/HistoricTileMapOverlay.swift
@@ -62,9 +62,8 @@ class HistoricTileMapOverlay: MKTileOverlay {
     let maxX = Int(floor((MKMapRectGetMaxX(rect) * Double(scale)) / kTileSize))
     let minY = Int(floor((MKMapRectGetMinY(rect) * Double(scale)) / kTileSize))
     let maxY = Int(floor((MKMapRectGetMaxY(rect) * Double(scale)) / kTileSize))
-    
-    for var x = minX; x <= maxX; x++  {
-      for var y = minY; y <= maxY; y++ {
+    for x in minX ..< maxX {
+      for y in minY ..< maxY {
         let flippedY = abs(y + 1 - tilesAtZ)
         let tileKey = "\(z)/\(x)/\(flippedY)"
         
